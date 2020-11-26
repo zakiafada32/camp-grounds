216d46fa5439fbbb49d9a3ac8ea3d7b3cba00597
zakiafada32 <afadazaki32@gmail.com>
2020-11-26 15:47:08 +0700

    add map to show page

--- seeds/index.js
+++ seeds/index.js
@@ -30,6 +30,10 @@ const seedDB = async () => {
       description:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
       price,
+      geometry: {
+        type: 'Point',
+        coordinates: [-113.1331, 47.0202],
+      },
       images: [
         {
           url:
